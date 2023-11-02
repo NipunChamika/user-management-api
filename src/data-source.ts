feb48e4c6e39e3bfb8651c2880d57bff85b00dc5
Nipun Chamika <108785832+NipunChamika@users.noreply.github.com>
2023-11-02 15:03:24 +0530

    Update data-source.ts

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,15 +1,18 @@
 import "reflect-metadata";
 import { createConnection, Connection } from "typeorm";
 import { User } from "./entity/User";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 export const initializeDatabaseConnection = async (): Promise<Connection> => {
     return createConnection({
         type: "mysql",
-        host: "localhost",
-        port: 3306,
-        username: "root",
-        password: "N1pun$",
-        database: "ts_company_typeorm",
+        host: process.env.MYSQL_HOST,
+        port: +process.env.MYSQL_PORT,
+        username: process.env.MYSQL_ROOT,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASE,
         synchronize: true,
         logging: false,
         entities: [User],
@@ -18,25 +21,3 @@ export const initializeDatabaseConnection = async (): Promise<Connection> => {
         subscribers: [],
     });
 }
-
-
-
-
-// import "reflect-metadata"
-// import { DataSource } from "typeorm"
-// import { User } from "./entity/User"
-
-// export const AppDataSource = new DataSource({
-//     type: "mysql",
-//     host: "localhost",
-//     port: 3306,
-//     username: "root",
-//     password: "N1pun$",
-//     database: "ts_company_typeorm",
-//     synchronize: true,
-//     logging: false,
-//     entities: [User],
-//     migrations: [__dirname + "/migration/**/*.ts"],
-//     migrationsTableName: "typeorm_migrations",
-//     subscribers: [],
-// })
